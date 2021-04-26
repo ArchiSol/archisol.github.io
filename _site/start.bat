060cc9dab01c55d081512e5de300ba09d877416a
Pinepond <songyeon0058@gmail.com>
2021-04-26 11:04:33 +0900

    gitignore 다시 적용

--- _site/start.bat
+++
@@ -1,2 +0,0 @@
-rem start local jekyll server
-bundle exec jekyll serve --port 8999 --trace
--- _site/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# start local jekyll server
-bundle exec jekyll serve --port 8999 --host 10.81.208.53 --trace
